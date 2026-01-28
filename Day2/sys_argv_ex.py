0d51e726727ba9a160cafd6f0a60338bd7138352
Meet <2402031030051@silveroakuni.ac.in>
2026-01-28 10:17:39 +0530

    day 2

--- Day2/sys_argv_ex.py
+++ Day2/sys_argv_ex.py
@@ -1,5 +1,6 @@
 import sys
 import os 
+import argparse
 
 # a = sys.argv[1]
 # b = sys.argv[2]
@@ -12,11 +13,32 @@ import os
 
 # print(a+b)
 
-path = sys.argv[1]
-files = os.listdir(path)
+# path = sys.argv[1]
+# files = os.listdir(path)
 
 # print(len(files))
-print(files)
+# print(files)
+
+# parser = argparse.ArgumentParser()
+
+# parser.add_argument("a", type=int)
+# parser.add_argument("b",type=int)
+
+
+# args = parser.parse_args()
+
+# print(args.a + args.b)
+
+
+parser = argparse.ArgumentParser(description="Count files in folder")
+parser.add_argument("path", help="Folder path")
+
+args = parser.parse_args()
+
+files = os.listdir(args.path)
+print("Total files:", len(files))
+
+
 
 
 
